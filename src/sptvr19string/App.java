a3f16dbf2ecd659a33ee5fc486d4b8673568bbd3
roman552 <57655645+roman552@users.noreply.github.com>
2020-09-22 21:15:24 +0300

    Update App.java

--- src/sptvr19string/App.java
+++ src/sptvr19string/App.java
@@ -12,45 +12,49 @@ package sptvr19string;
 class App {
     public void run() {
         System.out.println("---String---");
-        String text1 = "ac";
-        char[] text1Array = text1.toCharArray();
-        String text2 = "abc";
+        String text1 = "Съешь же ещё этих мягких французских булок да выпей чаю";
+        String text2 = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
         char[] text2Array = text2.toCharArray();
         boolean a = true;
-        System.out.println(text1Array);
-        System.out.println(text2Array);
-        for (int i = 0; i < text1Array.length;) {
-            for (int j = 0; j < text2Array.length;) {
-                if (text1Array[i]==text2Array[j]) {
+        System.out.println("Ваша строка: "+text1);
+        System.out.println("Русский алфавит: "+text2);
+        text1 = text1.replace(" ", "");
+        text1 = text1.replace(".", "");
+        text1 = text1.replace(",", "");
+        text1 = text1.toLowerCase();
+        char[] text1Array = text1.toCharArray();
+        int t = 0;
+        for (int i = 0; i < 33;) {
+            for (int j = 0; j < text1Array.length;) {
+                if (text2Array[i]==text1Array[j]) {
                     i++;
+                    t++;
                     j=0;
-                    a = true;
-                
-                    if (i==text1Array.length) {
-                        if (a==true) {
-                            System.out.println("строка содержит алфавит");
-                            break;
-                        } else {
-                            System.out.println("строка НЕ содержит алфавит");
+                    if (i==33) { 
+                        if (t==33) {
+                            System.out.println("Строка содержит все буквы русского алфавита!");
+                            a = false;
                             break;
                         }
                     }
-                } else {
-                    j++;
-                    a = false;
-                    if (j==text1Array.length) {
-                        if (a==true) {
-                            System.out.println("строка содержит алфавит");
-                            break;
-                        } else {
-                            System.out.println("строка НЕ содержит алфавит");
-                            break;
-                    
+                }
+                else {
+                    if (j+1 == text1Array.length) {
+                        System.out.println("Строка НЕ содержит все буквы русского алфавита!");
+                        a =false;
+                        break;                       
+                    }
+                    if (j<text1Array.length) {
+                        if (i <= 33) {
+                            j++;
+                        }
                     }
                 }
             }
+            if (a==false) {
+                System.out.println("---Конец программы---");
+                break;
+            }
         }
-        
-        
     }
 }
